2e442a62d22be82cef50d6019a1158fb4d750423
Amirhamja422 <amirhamja422@gmail.com>
2023-06-09 23:44:06 +0600

    csv download

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -128,7 +128,10 @@
   $(function () {
     $("#example1").DataTable({
       "responsive": true, "lengthChange": false, "autoWidth": false,
-      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+      // "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+      // csv download option 
+      "buttons": ["csv"]
+
     }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
     $('#example2').DataTable({
       "paging": true,
